9c90b22fccbc944d68f758b990ee7b8964533e66
D4rkCaptain <karimiali2004@gmail.com>
2023-01-26 10:20:59 -0500

    Saving

--- Main.java
+++ Main.java
@@ -5,18 +5,30 @@ class Main {
 
     Scanner scanner = new Scanner(System.in);
     System.out.println("Hello world!");
+    String item = "";
+    int counterWood = 1;
 
     while (true){
-      System.out.println("What Item would you like to add?(Type Wood for Wood)");
-      String item = scanner.nextLine();
-      if (item.equals("Wood")){
-        break;
-      } else {
-        System.out.println("Invalid input.");
+      while (true){
+        System.out.println("What Item would you like to add?(Type Wood for Wood)");
+        String commandItem = scanner.nextLine();
+        if (commandItem.equals("Wood")){
+          item = commandItem;
+          break;
+        } else {
+          System.out.println("Invalid input. Please try again");
+        }
+      }
+  
+      if (item.equals("Wood")) {
+        Woods wood = new Woods();
+        wood.setName("Wood " + counterWood);
+        System.out.println("A new Wood object named " + wood.getName() + " has been created.");
+        counterWood++;
       }
 
-      
-    }
+    } 
+    
 
   }
 }
