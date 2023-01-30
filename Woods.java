18627e2598f07944e39169c2125f57baad89684f
D4rkCaptain <karimiali2004@gmail.com>
2023-01-30 00:54:01 -0500

    save

--- Woods.java
+++ Woods.java
@@ -1,42 +1,5 @@
-public class Woods {
-    private String name;
-    String WoodType = "";
-    float WoodWeight = 0;
-    float WoodSpaceOccupied = 0;
-    float WoodValue = 0;
+public class Woods extends Item {
     public Woods(String type, float weight, float space, float value) {
-        WoodType = type;
-        WoodWeight = weight;
-        WoodSpaceOccupied = space;
-        WoodValue = value;
+        super(type, weight, space, value);
     }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setWoodType(String type) {
-        this.WoodType = type;
-    }
-    public void setWoodWeight(float weight) {
-        this.WoodWeight = weight;
-    }
-    public void setWoodSpaceOccupied(float space) {
-        this.WoodSpaceOccupied = space;
-    }
-    public void setWoodValue(float value) {
-        this.WoodValue = value;
-    }
-    public float getWoodWeight(){
-        return this.WoodWeight;
-    }
-    public float getWoodSpaceOccupied(){
-        return this.WoodSpaceOccupied;
-    }
-    public float getWoodValue(){
-        return this.WoodValue;
-    }
-
-  
 }
